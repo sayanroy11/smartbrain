c6022285a9082a964b223bbbe8fab8fcf4aaebdf
sayanroy11 <85430745+sayanroy11@users.noreply.github.com>
2021-07-12 00:32:35 +0530

    added responsive

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,8 +8,8 @@ const ImageLinkForm =({onInputChange,onbtnSubmit})=>{
 		</p>
 		<div className='center'>
 		<div className='form center pa4 br3 shadow-5'>
-		<input className='f4 pa2 w-70 center' placeholder='enter url' type='text' onChange={onInputChange}/>
-		<button className='w-30 br3 grow f4 link ph3 pv2 dib white bg-light-purple'
+		<input className='f4 pa2 w-80 center' placeholder='enter url' type='text' onChange={onInputChange}/>
+		<button className='ba grow f5 link ph3 pv2 dib white bg-light-purple'
 		onClick={onbtnSubmit}
 		>Detect</button>
 		</div>
