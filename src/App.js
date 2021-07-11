82745c64b9a44681b7c11c28d4c4e6bcc9b3ca5e
sayanroy11 <85430745+sayanroy11@users.noreply.github.com>
2021-07-11 22:37:26 +0530

    resolving

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component{
   }
   onbtnSubmit = () => {
     this.setState({imageurl: this.state.input});
-      fetch('https://ancient-harbor-94121.herokuapp.com//imageurl', {
+      fetch('https://ancient-harbor-94121.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component{
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://ancient-harbor-94121.herokuapp.com//image', {
+          fetch('https://ancient-harbor-94121.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
